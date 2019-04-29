krisfremen/lazarus-ccr

commit 8cd3f5784d29d01b53d16a4a883f7f78e1bd9c71

lazmapviewer: Fix painting of maps on Mac.
git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn@6884 8e941d3f-bd1b-0410-a28a-d453659cc2b4

components/lazmapviewer/source/mvmapviewer.pas
@@ -792,8 +792,11 @@ var
   rawImg: TRawImage;
 begin
   rawImg.Init;
-  //rawImg.Description.Init_BPP24_B8G8R8_BIO_TTB(AWidth, AHeight);
+  {$IFDEF WINDOWS}
   rawImg.Description.Init_BPP32_B8G8R8_BIO_TTB(AWidth, AHeight);
+  {$ELSE}
+  rawImg.Description.Init_BPP32_A8R8G8B8_BIO_TTB(AWidth, AHeight);
+  {$ENDIF}
   rawImg.CreateData(True);
   ABuffer := TLazIntfImage.Create(rawImg, true);
   ACanvas := TFPImageCanvas.Create(ABuffer);

commit 7ea250941efc77460f0d85cdaaeab9666e8b3e53

lazmapviewer: Fix image format error on Linux (introduced by r6884).
git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn@6886 8e941d3f-bd1b-0410-a28a-d453659cc2b4

components/lazmapviewer/source/mvmapviewer.pas
@@ -792,10 +792,10 @@ var
   rawImg: TRawImage;
 begin
   rawImg.Init;
-  {$IFDEF WINDOWS}
-  rawImg.Description.Init_BPP32_B8G8R8_BIO_TTB(AWidth, AHeight);
-  {$ELSE}
+  {$IFDEF DARWIN}
   rawImg.Description.Init_BPP32_A8R8G8B8_BIO_TTB(AWidth, AHeight);
+  {$ELSE}
+  rawImg.Description.Init_BPP32_B8G8R8_BIO_TTB(AWidth, AHeight);
   {$ENDIF}
   rawImg.CreateData(True);
   ABuffer := TLazIntfImage.Create(rawImg, true);