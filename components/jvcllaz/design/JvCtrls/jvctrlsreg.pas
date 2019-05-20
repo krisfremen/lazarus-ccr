c6528754b7a8cd1a5467079cd3e075b428a11560
wp_xxyyzz <wp_xxyyzz@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2019-05-20 22:49:48 +0000

    jvcllaz: Add TJvInstallLabel. Patch by Michal Gawrycki, issue #35604.
    git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn@6930 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- components/jvcllaz/design/JvCtrls/jvctrlsreg.pas
+++ components/jvcllaz/design/JvCtrls/jvctrlsreg.pas
@@ -16,7 +16,7 @@ implementation
 uses
   Classes, Controls, ActnList, PropEdits, JvDsgnConsts,
   JvMovableBevel, JvRuler, JvGroupHeader, JvRollOut,
-  JvHtControls, JvHint, JvHTHintForm, JvComboListBox,
+  JvHtControls, JvHint, JvHTHintForm, JvComboListBox, JvInstallLabel,
   JvAutoComplete;   // original JVCL has this in package JvCore
 
 procedure Register;
@@ -24,7 +24,7 @@ begin
   RegisterComponents(RsPaletteJvcl, [
     TJvMovableBevel, TJvMovablePanel, TJvRuler, TJvGroupHeader, TJvRollOut,
     TJvHint, TJvHTLabel, TJvHTListbox, TJvHTCombobox, TJvComboListBox,
-    TJvLookupAutoComplete
+    TJvLookupAutoComplete, TJvInstallLabel
   ]);
   RegisterPropertyEditor(TypeInfo(TCaption), TJvHTLabel, 'Caption', TJvHintProperty);
   RegisterActions(RsJVCLActionsCategory, [TJvRollOutAction], nil);
