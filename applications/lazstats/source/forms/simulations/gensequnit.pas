07d3c0c32a54a952bbc989223884dfda423792bb
wp_xxyyzz <wp_xxyyzz@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2020-05-08 12:13:59 +0000

    LazStats: Refactor GenSeqUnit. Add pdf to chm file.
    git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn@7438 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- applications/lazstats/source/forms/simulations/gensequnit.pas
+++ applications/lazstats/source/forms/simulations/gensequnit.pas
@@ -15,12 +15,11 @@ type
   { TGenSeqFrm }
 
   TGenSeqFrm = class(TForm)
+    Bevel1: TBevel;
     NoCasesEdit: TEdit;
-    Panel1: TPanel;
     ResetBtn: TButton;
-    CancelBtn: TButton;
     ComputeBtn: TButton;
-    ReturnBtn: TButton;
+    CloseBtn: TButton;
     LabelEdit: TEdit;
     Label3: TLabel;
     StartAtEdit: TEdit;
@@ -30,9 +29,9 @@ type
     RadioGroup1: TRadioGroup;
     StaticText1: TStaticText;
     procedure ComputeBtnClick(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormShow(Sender: TObject);
-    procedure NoCasesEditExit(Sender: TObject);
     procedure RadioGroup1Click(Sender: TObject);
     procedure ResetBtnClick(Sender: TObject);
   private
@@ -47,79 +46,120 @@ var
 
 implementation
 
+uses
+  Math;
+
 { TGenSeqFrm }
 
 procedure TGenSeqFrm.ResetBtnClick(Sender: TObject);
 begin
-    RadioGroup1.ItemIndex := 1;
-    NoCasesEdit.Text := '';
-    StartAtEdit.Text := '';
-    IncrEdit.Text := '';
-    LabelEdit.Text := '';
+  RadioGroup1.ItemIndex := 1;
+  NoCasesEdit.Text := '';
+  StartAtEdit.Text := '';
+  IncrEdit.Text := '';
+  LabelEdit.Text := '';
 end;
 
 procedure TGenSeqFrm.FormShow(Sender: TObject);
 begin
-    ResetBtnClick(self);
+  ResetBtnClick(self);
 end;
 
 procedure TGenSeqFrm.ComputeBtnClick(Sender: TObject);
 var
-    i, col : integer;
-    First, Increment : double;
+  i, col: integer;
+  First, Increment: double;
 begin
-    if StartAtEdit.Text = '' then
+  if RadioGroup1.ItemIndex = 1 then begin
+    if NoCasesEdit.Text = '' then
     begin
-        ShowMessage('Error! No starting value provided.');
-        exit;
+      NoCasesEdit.Setfocus;
+      MessageDlg('Number of cases to generate not specified.', mtError, [mbOK], 0);
+      exit;
     end;
-    if IncrEdit.Text = '' then
+    if not TryStrToInt(NoCasesEdit.Text, NCases) or (NCases <= 0) then
     begin
-        ShowMessage('Error! No increment value provided.');
-        exit;
+      NoCasesEdit.SetFocus;
+      MessageDlg('Number of cases must be a valid, positive integer.', mtError, [mbOK], 0);
+      exit;
     end;
-    if LabelEdit.Text = '' then
-    begin
-        ShowMessage('Error! No variable label provided.');
-        exit;
-    end;
-    if NoCases < Ncases then
-    begin
-        OS3MainFrm.DataGrid.RowCount := NCases + 1;
-        OS3MainFrm.NoCasesEdit.Text := IntToStr(NCases);
-        NoCases := Ncases;
-    end;
-    if NoVariables <= 0 then // a new data file
-    begin
-        OS3MainFrm.DataGrid.ColCount := 2;
-        OS3MainFrm.DataGrid.RowCount := Ncases + 1;
-        for i := 1 to Ncases do
-            OS3MainFrm.DataGrid.Cells[0,i] := format('Case %d',[i]);
-        col := 1;
-        DictionaryFrm.DictGrid.RowCount := 1;
-        DictionaryFrm.DictGrid.ColCount := 8;
-        DictionaryFrm.NewVar(col);
-        DictionaryFrm.DictGrid.Cells[1,col] := LabelEdit.Text;
-        OS3MainFrm.DataGrid.Cells[1,0] := LabelEdit.Text;
-        DictionaryFrm.DictGrid.RowCount := 2;
-        NoVariables := 1;
+  end else
+    NCases := NoCases;
 
-    end
-    else // existing data file
-    begin
-        col := NoVariables + 1;
-        DictionaryFrm.NewVar(col);
-        DictionaryFrm.DictGrid.Cells[1,col] := LabelEdit.Text;
-    end;
+  if (StartAtEdit.Text = '') or not TryStrToFloat(StartAtEdit.Text, First) then
+  begin
+    StartAtEdit.Setfocus;
+    MessageDlg('No starting value provided.', mtError, [mbOK], 0);
+    exit;
+  end;
 
-    First := StrToFloat(StartAtEdit.Text);
-    Increment := StrToFloat(IncrEdit.Text);
+  if (IncrEdit.Text = '') then
+  begin
+    IncrEdit.SetFocus;
+    MessageDlg('No increment value provided.', mtError, [mbOK], 0);
+    exit;
+  end;
+  if not TryStrToFloat(IncrEdit.Text, Increment) or (Increment <= 0) then
+  begin
+    IncrEdit.SetFocus;
+    MessageDlg('No valid increment value provided.', mtError, [mbOK], 0);
+    exit;
+  end;
+
+  if LabelEdit.Text = '' then
+  begin
+    LabelEdit.SetFocus;
+    MessageDlg('No variable label provided.', mtError, [mbOK], 0);
+    exit;
+  end;
+
+  if NoCases < Ncases then
+  begin
+    OS3MainFrm.DataGrid.RowCount := NCases + 1;
+    OS3MainFrm.NoCasesEdit.Text := IntToStr(NCases);
+    NoCases := Ncases;
+  end;
+
+  if NoVariables <= 0 then // a new data file
+  begin
+    OS3MainFrm.DataGrid.ColCount := 2;
+    OS3MainFrm.DataGrid.RowCount := Ncases + 1;
     for i := 1 to Ncases do
-    begin
-        OS3MainFrm.DataGrid.Cells[col,i] := format('%8.3f',[First]);
-        First := First + Increment;
-    end;
-    OS3MainFrm.NoVarsEdit.Text := IntToStr(NoVariables);
+      OS3MainFrm.DataGrid.Cells[0,i] := format('Case %d',[i]);
+    col := 1;
+    DictionaryFrm.DictGrid.RowCount := 1;
+    DictionaryFrm.DictGrid.ColCount := 8;
+    DictionaryFrm.NewVar(col);
+    DictionaryFrm.DictGrid.Cells[1,col] := LabelEdit.Text;
+    OS3MainFrm.DataGrid.Cells[1,0] := LabelEdit.Text;
+    DictionaryFrm.DictGrid.RowCount := 2;
+    NoVariables := 1;
+  end
+  else // existing data file
+  begin
+    col := NoVariables + 1;
+    DictionaryFrm.NewVar(col);
+    DictionaryFrm.DictGrid.Cells[1,col] := LabelEdit.Text;
+  end;
+
+  First := StrToFloat(StartAtEdit.Text);
+  Increment := StrToFloat(IncrEdit.Text);
+  for i := 1 to Ncases do
+  begin
+    OS3MainFrm.DataGrid.Cells[col,i] := Format('%.3f', [First]);
+    First := First + Increment;
+  end;
+  OS3MainFrm.NoVarsEdit.Text := IntToStr(NoVariables);
+end;
+
+procedure TGenSeqFrm.FormActivate(Sender: TObject);
+var
+  w: Integer;
+begin
+  w := MaxValue([ResetBtn.Width, ComputeBtn.Width, CloseBtn.Width]);
+  ResetBtn.Constraints.MinWidth := w;
+  ComputeBtn.Constraints.MinWidth := w;
+  CloseBtn.Constraints.MinWidth := w;
 end;
 
 procedure TGenSeqFrm.FormCreate(Sender: TObject);
@@ -129,28 +169,18 @@ begin
     Application.CreateForm(TDictionaryFrm, DictionaryFrm);
 end;
 
-procedure TGenSeqFrm.NoCasesEditExit(Sender: TObject);
-begin
-    if RadioGroup1.ItemIndex = 1 then Ncases := StrToInt(NoCasesEdit.Text);
-    if (Ncases <= 0) and (RadioGroup1.ItemIndex = 1) then
-    begin
-        ShowMessage('Error!  No. of cases to generate not specified.');
-        exit;
-    end;
-end;
-
 procedure TGenSeqFrm.RadioGroup1Click(Sender: TObject);
 begin
-    if RadioGroup1.ItemIndex = 0 then
+  if RadioGroup1.ItemIndex = 0 then
+  begin
+    if NoCases <= 0 then
     begin
-        if NoCases <= 0 then
-        begin
-            ShowMessage('Error! There are currently no cases!');
-            exit;
-        end
-        else Ncases := NoCases;
-    end
-    else NoCasesEdit.SetFocus;
+      MessageDlg('There are currently no cases.', mtError, [mbOK], 0);
+      exit;
+    end else
+      Ncases := NoCases;
+  end else
+    NoCasesEdit.SetFocus;
 end;
 
 initialization
